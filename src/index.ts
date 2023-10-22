c8adec473d81eeb017289661bf8448236492ca85
sacuadros09 <sacuma09@gmail.com>
2023-10-22 11:09:01 -0500

    Correction index.ts screens

--- src/index.ts
+++ src/index.ts
@@ -24,27 +24,32 @@ class AppContainer extends HTMLElement {
         if (this.shadowRoot) this.shadowRoot.innerHTML = "";
         switch (appState.screen){
             case Screens.REGISTER:
-                const register = this.ownerDocument.createElement('my-dashboardprofile');
+                const register = this.ownerDocument.createElement('my-dashboardregister');
                 this.shadowRoot?.appendChild(register);
                 break;
 
+                case Screens.LOGIN:
+                const login = this.ownerDocument.createElement('my-dashboardlogin');
+                this.shadowRoot?.appendChild(login);
+                break;
+
                 case Screens.DASHBOARD:
-                const dashboard = this.ownerDocument.createElement('my-dashboardmenu');
+                const dashboard = this.ownerDocument.createElement('my-dashboard');
                 this.shadowRoot?.appendChild(dashboard);
                 break;
 
                 case Screens.PROFILE:
-                    const profile = this.ownerDocument.createElement("");
+                    const profile = this.ownerDocument.createElement("my-dashboardprofile");
                     this.shadowRoot?.appendChild(profile);
                     break;
                     
                     case Screens.LANDING:
-                    const landing = this.ownerDocument.createElement("");
+                    const landing = this.ownerDocument.createElement("my-dashboardlanding");
                     this.shadowRoot?.appendChild(landing);
                     break;
 
                     case Screens.MENU:
-                        const menu = this.ownerDocument.createElement("");
+                        const menu = this.ownerDocument.createElement("my-dashboardmenu");
                         this.shadowRoot?.appendChild(menu);
                         break;
 
