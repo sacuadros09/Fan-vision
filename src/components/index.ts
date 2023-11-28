e09009912e6f3016d4da41766cd00aa0d2abf312
sacuadros09 <sacuma09@gmail.com>
2023-11-28 11:45:04 -0500

    export uploadepost

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,7 +8,7 @@ export {default as Login} from "./Login/Login"
 export {default as Menu} from "./Menu/Menu"
 export {default as Profile} from "./Profile/Profile"
 export {default as Register} from "./Register/Register"
-export {default as Post} from "./Post/Post"
+export {default as UploadePost} from "./Post/Post"
 export {default as FormLogin} from "./FormLogin/FormLogin"
 export {default as FormRegister} from "./FormRegister/FormRegister"
  
